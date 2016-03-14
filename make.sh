cba5edf1c9bd285c3956a2e2b04033258d9dc299
zxv5 <254790952@qq.com>
2016-03-14 21:31:30 +0800

    route and bypass diff make

--- make.sh
+++ make.sh
@@ -2,6 +2,8 @@
 
 cp build_config/build.config .config
 
+DATESTR=`date +%Y%m%d%H%M`
+
 for i in build_config/*.conf; do
 . $i
 echo $board
@@ -22,7 +24,17 @@ VERSION=`cat .config | grep CONFIG_VERSION_NUMBER | awk -F\" '{print $2}'`
 
 ls bin/ramips/*.bin | grep -v puppies | xargs rm >/dev/null 2>&1
 mkdir -p bin/ramips/AC
-echo mv bin/ramips/*$VERSION*puppies*bin bin/ramips/AC/$DIST-$VERSION
-mv bin/ramips/*$VERSION*puppies*bin bin/ramips/AC/$DIST-$VERSION
+echo mv bin/ramips/*$VERSION*puppies*bin bin/ramips/AC/UploadBrush-bin.img
+mv bin/ramips/*$VERSION*puppies*bin bin/ramips/AC/UploadBrush-bin.img
+
+BINMD5=`md5sum bin/ramips/AC/UploadBrush-bin.img | awk '{print $1}'`
+BINRANDOM=$bin_random
+RANDMD5=`echo -n $BINRANDOM | md5sum | awk '{print $1}'`
+echo -n ${BINMD5}${RANDMD5} | md5sum | awk '{print $1}' >  bin/ramips/AC/bin_random.txt
+cd  bin/ramips/AC/
+tar -zcf $DIST-$VERSION".tar.gz" UploadBrush-bin.img bin_random.txt
+rm UploadBrush-bin.img bin_random.txt
+mv $DIST-$VERSION".tar.gz" $DIST-$VERSION
+cd -
 
 done
